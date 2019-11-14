a5ed8c568be3a4eeae3a54c323b593369f41e6ba
markofsuccess <markussjoman1@gmail.com>
2019-11-14 13:44:23 +0000

    created addtask.html file with contact form, and added app.route for addtask.html

--- app.py
+++ app.py
@@ -14,6 +14,10 @@ mongo = PyMongo(app)
 @app.route('/get_tasks')
 def get_tasks():
     return render_template("tasks.html", tasks=mongo.db.tasks.find())
+    
+@app.route('/add_task')
+def add_task():
+    return render_template('addtask.html')
 
 
 if __name__ == '__main__':
