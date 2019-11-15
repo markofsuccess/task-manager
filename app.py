a9ca18ac03df95d05525e432eb67c9b2cbfd0a61
markofsuccess <markussjoman1@gmail.com>
2019-11-15 14:42:52 +0000

    added addcategory to categories button and function

--- app.py
+++ app.py
@@ -60,6 +60,11 @@ def get_categories():
     return render_template('categories.html',
                            categories=mongo.db.categories.find())
 
+@app.route('/delete_category/<category_id>')
+def delete_category(category_id):
+    mongo.db.categories.remove({'_id': ObjectId(category_id)})
+    return redirect(url_for('get_categories'))                           
+
 @app.route('/edit_category/<category_id>')
 def edit_category(category_id):
     return render_template('editcategory.html',
@@ -73,10 +78,16 @@ def update_category(category_id):
         {'category_name': request.form.get('category_name')})
     return redirect(url_for('get_categories'))
     
-@app.route('/delete_category/<category_id>')
-def delete_category(category_id):
-    mongo.db.categories.remove({'_id': ObjectId(category_id)})
+    
+@app.route('/insert_category', methods=['POST'])
+def insert_category():
+    category_doc = {'category_name': request.form.get('category_name')}
+    mongo.db.categories.insert_one(category_doc)
     return redirect(url_for('get_categories'))
+    
+@app.route('/add_category')
+def add_category():
+    return render_template('addcategory.html')
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
